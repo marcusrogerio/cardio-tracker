a19cdbf522dcff2a235ecff6946387f9a2af11c9
rursu <roman.ursu@perfectial.com>
2016-07-20 18:50:26 +0300

    developing UI

--- app/src/main/java/com/romanus/cardiotracker/ui/workout/WorkoutFragment.java
+++ app/src/main/java/com/romanus/cardiotracker/ui/workout/WorkoutFragment.java
@@ -3,16 +3,34 @@ package com.romanus.cardiotracker.ui.workout;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.ListView;
+import android.widget.TextView;
 
 import com.romanus.cardiotracker.R;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 /**
  * Created by rursu on 20.07.16.
  */
 public class WorkoutFragment extends Fragment {
+
+    @BindView(R.id.lv_parameters_list)
+    ListView paramenetrsListView;
+
+    private Map<WorkoutParameter, String> values = new HashMap<>();
 
     public static WorkoutFragment newInstance() {
 
@@ -27,6 +45,67 @@ public class WorkoutFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_workout, container, false);
+        ButterKnife.bind(this, view);
+
+        initList();
         return view;
+    }
+
+    private void initList() {
+        ParametersAdapter parametersAdapter = new ParametersAdapter();
+        paramenetrsListView.setAdapter(parametersAdapter);
+    }
+
+    class ParametersAdapter extends BaseAdapter {
+
+        @Override
+        public int getCount() {
+            return WorkoutParameter.values().length;
+        }
+
+        @Override
+        public Object getItem(int position) {
+            return WorkoutParameter.values()[position];
+        }
+
+        @Override
+        public long getItemId(int position) {
+            return 0;
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+            if (convertView == null) {
+                convertView = LayoutInflater.from(parent.getContext()).inflate(R.layout.workout_parameter_item, parent, false);
+            }
+
+            final WorkoutParameter workoutParameter = (WorkoutParameter) getItem(position);
+
+            ImageView icon = (ImageView) convertView.findViewById(R.id.iv_icon);
+            final TextView title = (TextView) convertView.findViewById(R.id.tv_title);
+            EditText value = (EditText) convertView.findViewById(R.id.et_value);
+
+            value.addTextChangedListener(new TextWatcher() {
+                @Override
+                public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+                }
+
+                @Override
+                public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+                }
+
+                @Override
+                public void afterTextChanged(Editable s) {
+                    values.put(workoutParameter, s.toString());
+                }
+            });
+
+            icon.setImageResource(workoutParameter.getIcon());
+            title.setText(workoutParameter.getTitle());
+
+            return convertView;
+        }
     }
 }
--- app/src/main/java/com/romanus/cardiotracker/ui/workout/WorkoutParameter.java
+++ app/src/main/java/com/romanus/cardiotracker/ui/workout/WorkoutParameter.java
@@ -9,7 +9,7 @@ public enum WorkoutParameter {
     MIN_HR("Min HR", R.drawable.heart_24),
     MAX_HR("Max HR", R.drawable.heart_thunder_24),
     TARGET_DURATION("Duration", R.drawable.time_24),
-    TRAGET_DISTANCE("Distance", R.drawable.length_24);
+    TARGET_DISTANCE("Distance", R.drawable.length_24);
 
     private String title;
     private int icon;
