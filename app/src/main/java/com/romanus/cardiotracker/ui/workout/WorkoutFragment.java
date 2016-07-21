3de9821d58d5e63464362b1c4b4338fbf64fbe57
rursu <roman.ursu@perfectial.com>
2016-07-21 13:12:48 +0300

    UI issues resolved

--- app/src/main/java/com/romanus/cardiotracker/ui/workout/WorkoutFragment.java
+++ app/src/main/java/com/romanus/cardiotracker/ui/workout/WorkoutFragment.java
@@ -3,16 +3,10 @@ package com.romanus.cardiotracker.ui.workout;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
-import android.text.Editable;
-import android.text.TextWatcher;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.BaseAdapter;
 import android.widget.EditText;
-import android.widget.ImageView;
-import android.widget.ListView;
-import android.widget.TextView;
 
 import com.romanus.cardiotracker.R;
 
@@ -27,85 +21,35 @@ import butterknife.ButterKnife;
  */
 public class WorkoutFragment extends Fragment {
 
-    @BindView(R.id.lv_parameters_list)
-    ListView paramenetrsListView;
+    @BindView(R.id.et_value_min_heart_value)
+    EditText minHeartEditText;
 
-    private Map<WorkoutParameter, String> values = new HashMap<>();
+    @BindView(R.id.et_value_max_heart_value)
+    EditText maxHeartEditText;
+
+    @BindView(R.id.et_value_duration)
+    EditText durationEditText;
+
+    @BindView(R.id.et_value_distance)
+    EditText distanceEditText;
 
     public static WorkoutFragment newInstance() {
 
         Bundle args = new Bundle();
-        
+
         WorkoutFragment fragment = new WorkoutFragment();
         fragment.setArguments(args);
         return fragment;
     }
-    
+
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_workout, container, false);
         ButterKnife.bind(this, view);
 
-        initList();
         return view;
     }
 
-    private void initList() {
-        ParametersAdapter parametersAdapter = new ParametersAdapter();
-        paramenetrsListView.setAdapter(parametersAdapter);
-    }
 
-    class ParametersAdapter extends BaseAdapter {
-
-        @Override
-        public int getCount() {
-            return WorkoutParameter.values().length;
-        }
-
-        @Override
-        public Object getItem(int position) {
-            return WorkoutParameter.values()[position];
-        }
-
-        @Override
-        public long getItemId(int position) {
-            return 0;
-        }
-
-        @Override
-        public View getView(int position, View convertView, ViewGroup parent) {
-            if (convertView == null) {
-                convertView = LayoutInflater.from(parent.getContext()).inflate(R.layout.workout_parameter_item, parent, false);
-            }
-
-            final WorkoutParameter workoutParameter = (WorkoutParameter) getItem(position);
-
-            ImageView icon = (ImageView) convertView.findViewById(R.id.iv_icon);
-            final TextView title = (TextView) convertView.findViewById(R.id.tv_title);
-            EditText value = (EditText) convertView.findViewById(R.id.et_value);
-
-            value.addTextChangedListener(new TextWatcher() {
-                @Override
-                public void beforeTextChanged(CharSequence s, int start, int count, int after) {
-
-                }
-
-                @Override
-                public void onTextChanged(CharSequence s, int start, int before, int count) {
-
-                }
-
-                @Override
-                public void afterTextChanged(Editable s) {
-                    values.put(workoutParameter, s.toString());
-                }
-            });
-
-            icon.setImageResource(workoutParameter.getIcon());
-            title.setText(workoutParameter.getTitle());
-
-            return convertView;
-        }
-    }
 }
